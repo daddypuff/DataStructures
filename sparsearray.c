7452fd42e812345638c92e1580bde28652140a8a
sanyam-11 <55022098+sanyam-11@users.noreply.github.com>
2020-01-21 19:01:35 +0530

    Create sparsearray.c
    Specially for C

---
+++ sparsearray.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int i, j;
+
+int main(){
+
+     int nonzero=0, matrix[10][10], array[26][3];
+     int mrows, mcolumn, arows ;
+     int x=1;
+
+     printf("Enter number of rows in matrix: ");
+     scanf("%d", &mrows);
+
+     printf("Enter number of columns in matrix: ");
+     scanf("%d", &mcolumn);
+
+     printf("Enter Elements of the Matrix- ");
+//*Scanning Matrix And determining number of Non Zero Elements  *//
+     for(i=0;i<mrows;i++){
+        for(j=0;j<mcolumn;j++){
+            scanf("%d", &matrix[i][j]);
+            if(matrix[i][j]!=0){nonzero++;}
+        }
+            }
+//*Checking if a sparse Matrix*//
+     if(nonzero>mrows*mcolumn/4){
+        printf("This cannot be considered and/or transformed into a scarce Matrix!!!");
+        exit(0);
+     }
+//*Else*//
+     arows = nonzero+1;
+    //*Value Assignment for the Scarce Array*//
+
+    array[0][0]=mrows;
+    array[0][1]=mcolumn;
+    array[0][2]=nonzero;
+
+    for(i=0;i<mrows;i++){
+        for(j=0;j<mcolumn;j++){
+            if(matrix[i][j]!=0){
+                array[x][0]= i;
+                array[x][1]=j;
+                array[x][2]=matrix[i][j];
+                x++;
+            }
+        }
+    }
+
+//*Resultant Array*//
+    printf("\n\nThis is the Scarce Array- \n");
+
+    for(i=0;i<arows;i++){
+        for(j=0;j<3;j++){
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+ }
